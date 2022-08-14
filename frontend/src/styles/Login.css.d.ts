c9401b133141d222a83eaaa6a99629f84432ba18
Oleh Komarnytskyi <oleh.komarnytskyi@dxc.com>
2022-08-15 00:46:15 +0300

    Front: remove rudiments 2

--- frontend/src/styles/Login.css.d.ts
+++ frontend/src/styles/Login.css.d.ts
@@ -3,6 +3,4 @@ export const stickRight: string;
 export const stickCntr: string;
 export const delimiter: string;
 export const fieldIcon: string;
-export const pwdShow: string;
-export const pwdHide: string;
 export const warnContainer: string;
