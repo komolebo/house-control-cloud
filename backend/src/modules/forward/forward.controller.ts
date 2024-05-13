edc05c65bda32a08980847b74647fee05d4b0ca5
Oleh Komarnytskyi <oleh.komarnytskyi@dxc.com>
2024-05-13 03:22:49 +0300

    Backend: Fix response headers in Cloud2Tablet comm

--- backend/src/modules/forward/forward.controller.ts
+++ backend/src/modules/forward/forward.controller.ts
@@ -1,21 +1,28 @@
-import {Controller, Get, Header, Param, Res} from '@nestjs/common';
+import {Controller, Get, Param, Res} from '@nestjs/common';
 import {ForwardService} from './forward.service';
 
 @Controller('api/tablet')
 export class ForwardController {
     constructor(private readonly forwardService: ForwardService) {}
 
+    TABLET_BACKEND_ADDR = `http://192.168.50.85:3000`;
+
     @Get('forward')
     async getPage() {
-        return await this.forwardService.forwardRequest('http://192.168.50.85:3000');
+        return await this.forwardService.forwardRequest(this.TABLET_BACKEND_ADDR);
     }
 
     @Get('forward/:res_folder/:res')
     async getStaticResources(@Param('res_folder') res_folder: string,
                              @Param('res') resource: string,
                              @Res() response) {
-        const imageData =
-            await this.forwardService.forwardStaticRequest(`http://192.168.50.85:3000/${res_folder}/${resource}`)
+        const [imageData, responseHeaders] =
+            await this.forwardService.forwardStaticRequest(`${this.TABLET_BACKEND_ADDR}/${res_folder}/${resource}`)
+
+        for (const [key, value] of Object.entries(responseHeaders)) {
+            response.set(key, value); // Set header on NestJS response
+        }
+
         response.send(imageData); // Send image data as Buffer
     }
 }
--- backend/src/modules/forward/forward.service.ts
+++ backend/src/modules/forward/forward.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, Logger} from '@nestjs/common';
-import {AxiosResponse} from "axios";
+import {AxiosHeaders, AxiosHeaderValue, AxiosResponse, AxiosResponseHeaders, RawAxiosResponseHeaders} from "axios";
 import {HttpService} from '@nestjs/axios';
 
 @Injectable()
@@ -11,16 +11,17 @@ export class ForwardService {
     async forwardRequest(url: string, data?: any): Promise<AxiosResponse> {
         this.logger.log(`Processing forward request to'${url}'`)
 
-        const response = await this.httpService.get(url, data).toPromise(); // Await Promise
+        const response = await this.httpService.get(url, data).toPromise();
         return response.data;
     }
 
-    async forwardStaticRequest(url: string, data?: any): Promise<AxiosResponse> {
+    async forwardStaticRequest(url: string, data?: any): Promise<[AxiosResponse, RawAxiosResponseHeaders]> {
         this.logger.log(`Processing forward request to static '${url}'`)
 
         const response = await this.httpService.get(url, {
                 responseType: 'arraybuffer'
             }).toPromise()
-        return response.data
+
+        return [response.data, response.headers]
     }
 }
